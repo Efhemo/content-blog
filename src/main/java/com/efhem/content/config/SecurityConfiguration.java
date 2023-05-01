6f6a8f0a8a2a86791b34c31de44a051ace784963
Femi Adegbite <efhem@Femis-MacBook-Pro.local>
2023-05-01 15:00:16 +0100

    implement securityfilter chain

---
+++ src/main/java/com/efhem/content/config/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.efhem.content.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final AuthenticationProvider authenticationProvider;
+    private final JWTAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity https) throws Exception {
+        https
+                //Disable csrf
+                .csrf().disable()
+                .authorizeHttpRequests(
+                  authorizeRequest ->
+                    //Permit some url that doesn't need authentication
+                    authorizeRequest.requestMatchers("/api/v1/auth").permitAll()
+                    //The rest of the url requires authentication
+                    .anyRequest().authenticated()
+                )
+                //STATELESS because we are authenticating once per request
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return https.build();
+    }
+}
